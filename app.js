9d70521bae6d82fe219771754c4ad422c7326a4f
Rajath <rajathinuganti@gmail.com>
2019-08-04 00:15:08 +0000

    Semi-Final version of Code Blogging Website

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 var Campground = require('./models/campgrounds');
 var seedDB = require('./seeds');
 var Comment = require('./models/comment');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var User = require('./models/user');
 
 seedDB();
 
@@ -24,6 +27,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+app.use(require('express-session')({
+	secret: "This is the YelpCamp server side code frameworks",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//you can do this to send currentUser to every page that has a navbar
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	next();
+});
+
+//or you could just do:
+// res.render("campgrounds/campgrounds", {campgrounds: allCampgrounds, currentUser: req.user});
+// in every route that has a navbar.
 
 
 //RESTful Routes
@@ -68,7 +95,6 @@ app.get("/campgrounds/:id", function(req, res){
 			console.log(err);
 		}
 		else {
-			console.log(foundCampground);
 			res.render("campgrounds/show", {campground: foundCampground});
 		}
 	});
@@ -76,13 +102,13 @@ app.get("/campgrounds/:id", function(req, res){
 
 
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggenIn,function(req, res){
 	Campground.findById(req.params.id, function(err, foundCampground){
 		res.render("comments/new", {campground: foundCampground});
 	});	
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggenIn,function(req, res){
 	Campground.findById(req.params.id, function(err, campground){
 		if(err){
 			console.log(err);
@@ -101,6 +127,58 @@ app.post("/campgrounds/:id/comments", function(req, res){
 		}
 	});
 });
+
+//Auth - RESTful routes
+
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		else{
+			passport.authenticate("local")(req, res, function(){
+				res.redirect("/campgrounds");
+			});
+		}
+	});
+});
+
+//show login form
+
+app.get("/login", function(req, res){
+	res.render("login");
+});
+
+// when a post request is received the middleware is first executed before the callback. the middleware here is checking if the user is successully authenticated or not. if yes he is taken to the page intended or if not the user stays back on the login page.
+app.post("/login", passport.authenticate("local", {
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}),
+	function(req, res){
+	
+});
+
+app.get("/logout", function(req, res, next){
+	req.logout();
+	res.redirect("/campgrounds");
+});
+
+//middleware for verifying if a user is logged in or not. if not it redirects to login page.
+function isLoggenIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	else{
+		res.redirect("/login");
+	}
+}
+
 
 
 app.listen(3000, process.env.IP, function(){
